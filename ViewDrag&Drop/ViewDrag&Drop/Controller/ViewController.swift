5edcbd2d7b6e446d62ff9d4c3c9f21cd7c061eac
MD Murad Hossain <mdmuradhossainmiraj@gmail.com>
2022-11-22 00:40:51 +0600

    update viewcontroller

--- ViewDrag&Drop/ViewDrag&Drop/Controller/ViewController.swift
+++ ViewDrag&Drop/ViewDrag&Drop/Controller/ViewController.swift
@@ -3,49 +3,51 @@
 //  ViewDrag&Drop
 //
 //  Created by Md Murad Hossain on 21/11/22.
-//
+
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
-    
+
 
     let myView = UIView(frame: CGRect(x: 0,
                                       y: 0,
                                       width: 380,
                                       height: 380)
     )
-    
+
     let myView2 = UIView(frame: CGRect(x: 0,
                                        y: 0,
                                        width: 150,
                                        height: 150)
     )
 
-    
+
     //let imageView = UIImageView(image: UIImage(named: "m2")!)
-    
+
     var isDraging =  false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         myViewConfig()
         view2Config()
-        
+
     }
-    
+
 //    MARK: View Configuration
-    
+
     func myViewConfig() {
-        
+
         myView.backgroundColor = .cyan
         myView.center = view.center
         view.addSubview(myView)
     }
-    
-    
+
+
     func view2Config() {
 
         myView2.backgroundColor = .orange
@@ -53,7 +55,7 @@ class ViewController: UIViewController {
         view.addSubview(myView2)
 
     }
-    
+
 }
 
 // MARK: View Drag and Drop setup
@@ -92,3 +94,53 @@ extension ViewController {
 
 
 
+
+// MARK: Second Step
+
+//
+//import UIKit
+//
+//class ViewController: UIViewController {
+//
+//    @IBOutlet weak var view11: UIView!
+//
+//    let movableView = UIView(frame: CGRect(x: 0, y: 0 , width: 200, height: 200))
+//
+//    var beginningPosition: CGPoint = .zero
+//    var initialMovableViewPosition: CGPoint = .zero
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        addMovableViews()
+//
+//    }
+//
+//    private func addMovableViews() {
+//
+//        movableView.backgroundColor = .green
+//        movableView.center = view11.center
+//        view.addSubview(movableView)
+//        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(touched(_:)))
+//        movableView.addGestureRecognizer(gestureRecognizer)
+//    }
+//
+//
+//
+//    @objc private func touched(_ gestureRecognizer: UIGestureRecognizer) {
+//        if let touchedView = gestureRecognizer.view {
+//            if gestureRecognizer.state == .began {
+//                beginningPosition = gestureRecognizer.location(in: touchedView)
+//                initialMovableViewPosition = view11.frame.origin
+//            } else if gestureRecognizer.state == .ended {
+//                touchedView.frame.origin = touchedView.frame.origin
+//            } else if gestureRecognizer.state == .changed {
+//                let location = gestureRecognizer.location(in: view)
+//                movableView.frame.origin.x = location.x - (movableView.frame.size.width/2)
+//                movableView.frame.origin.y = location.y - (movableView.frame.size.height/2)
+//
+//            }
+//        }
+//    }
+//
+//}
